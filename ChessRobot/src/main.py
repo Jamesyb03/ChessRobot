3eae9e0692dc0e2916f1ceccf8c0161b58db848b
Jamesyb03 <113983687+Jamesyb03@users.noreply.github.com>
2025-04-11 11:54:10 +0100

    Start

---
+++ ChessRobot/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       jbate                                                        #
+# 	Created:      4/11/2025, 11:51:58 AM                                       #
+# 	Description:  EXP project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello EXP")
+
+
+        
